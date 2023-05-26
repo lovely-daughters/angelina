51306a4fa9ed81bcfbec7c559564fa28a41f0d56
0eti <samuelzhang012@gmail.com>
2023-05-26 16:48:46 -0700

    angelina kernel

---
+++ src/kernel.py
@@ -0,0 +1,31 @@
+from ipykernel.kernelbase import Kernel
+
+
+class Angelina(Kernel):
+    implementation = "Angelina"
+    implementation_version = "0.5.14"
+    language = "javascript"
+    language_version = "ES6"
+    language_info = {
+        "name": "JavaScript",
+        "mimetype": "application/javascript",
+        "file_extension": ".js",
+    }
+    banner = "Angelina Ajimu
+
+    def do_execute(
+        self, code, silent, store_history=True, user_expressions=None, allow_stdin=False
+    ):
+        if not silent:
+            # stream_content = {"name": "stdout", "text": code}
+            # self.send_response(self.iopub_socket, "stream", stream_content)
+            stream_content = {"name": "stdout", "text": "😭"}
+            self.send_response(self.iopub_socket, "stream", stream_content)
+
+        return {
+            "status": "ok",
+            # The base class increments the execution count
+            "execution_count": self.execution_count,
+            "payload": [],
+            "user_expressions": {},
+        }
