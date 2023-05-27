f3bb0cc8164c57f37dcf478941ea251175cb8c1f
0eti <samuelzhang012@gmail.com>
2023-05-26 18:34:13 -0700

    trying to find a way to directly pipe outputs into chrome console

--- angelina_kernel/kernel.py
+++ angelina_kernel/kernel.py
@@ -2,6 +2,7 @@ from ipykernel.kernelbase import Kernel
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
+
 import pprint
 
 class Angelina(Kernel):
@@ -31,24 +32,37 @@ class Angelina(Kernel):
     def do_execute(
         self, code: str, silent, store_history=True, user_expressions=None, allow_stdin=False
     ):
-        # shitty cell magicks
-        if code.startswith(r"%tabs"):
-            self.send_response(self.iopub_socket, "stream", {"name": "stdout", "text": f"{self.pformat(self.driver.window_handles)}"})
-        elif code.startswith(r"%switch"):
-            tab_num = int(code.split(" ").pop())
-            self.driver.switch_to.window(self.driver.window_handles[tab_num])
-            self.send_response(self.iopub_socket, "stream", {"name": "stdout", "text": f"SWITCHED TO TAB: {tab_num}"})
-        elif code.startswith(r"%type"):
-            self.send_response(self.iopub_socket, "stream", {"name": "stdout", "text": type(code)})
-        elif not silent:
-            stream_content = {"name": "stdout", "text": f"😭 {self.sanity}"}
-            self.send_response(self.iopub_socket, "stream", stream_content)
-
+        try:
+            # shitty cell magicks
+            if code.startswith(r"%tabs"):
+                self.send_response(self.iopub_socket, "stream", {"name": "stdout", "text": f"{self.pformat(self.driver.window_handles)}"})
+            elif code.startswith(r"%switch"):
+                tab_num = int(code.split(" ").pop())
+                self.driver.switch_to.window(self.driver.window_handles[tab_num])
+                self.send_response(self.iopub_socket, "stream", {"name": "stdout", "text": f"SWITCHED TO TAB: {tab_num}"})
+            elif code.startswith(r"%type"):
+                self.send_response(self.iopub_socket, "stream", {"name": "stdout", "text": f"{type(code)}"})
+            elif not silent:
+                # script = f"return ({code})"
+                script = f"{code}"
+                result = self.driver.execute_script(script)
+                stream_content = {"name": "stdout", "text": f"{self.pformat(result)}"}
+                self.send_response(self.iopub_socket, "stream", stream_content)
+        except Exception as e:
+            content = {
+                "ename": type(e).__name__,
+                "evalue": str(e),
+                "traceback": []
+            }
+            self.send_response(self.iopub_socket, "error", content)
+            
         return {
             "status": "ok",
-            # The base class increments the execution count
             "execution_count": self.execution_count,
             "payload": [],
             "user_expressions": {},
         }
         
+    def _at_shutdown(self):
+        # self.driver.detach()
+        pass
